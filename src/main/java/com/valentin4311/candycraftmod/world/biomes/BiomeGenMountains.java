3004470c3fd293dad6510d6a0c774d6fda79fc2c
FlashFyre <33395370+Fla5hFyre@users.noreply.github.com>
2018-09-07 21:22:32 +0100

    Ported mountain biome - tweak others like this

--- src/main/java/com/valentin4311/candycraftmod/world/biomes/BiomeGenMountains.java
+++ src/main/java/com/valentin4311/candycraftmod/world/biomes/BiomeGenMountains.java
@@ -1,23 +1,22 @@
-package com.valentin4311.candycraftmod.world.biomes;
+package com.valentin4311.candycraft.world.biomes;
 
 import java.awt.Color;
 
-import com.valentin4311.candycraftmod.entity.EntityBunny;
-import com.valentin4311.candycraftmod.entity.EntityCandyPig;
-import com.valentin4311.candycraftmod.entity.EntityCandyWolf;
-import com.valentin4311.candycraftmod.entity.EntitySuguard;
-import com.valentin4311.candycraftmod.entity.EntityWaffleSheep;
+import com.valentin4311.candycraft.entity.EntityBunny;
+import com.valentin4311.candycraft.entity.EntityCandyPig;
+import com.valentin4311.candycraft.entity.EntityCandyWolf;
+import com.valentin4311.candycraft.entity.EntitySuguard;
+import com.valentin4311.candycraft.entity.EntityWaffleSheep;
 
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.biome.Biome;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BiomeGenMountains extends BiomeGenCandy
 {
-	public BiomeGenMountains(Biome.BiomeProperties properties)
+	public BiomeGenMountains()
 	{
-		super(properties);
+		super(new BiomeProperties("Sugar Mountains").setBaseHeight(0.5F).setHeightVariation(0.8F));
 		spawnableCreatureList.add(new SpawnListEntry(EntityCandyWolf.class, 12, 4, 4));
 		spawnableMonsterList.add(new SpawnListEntry(EntitySuguard.class, 4, 4, 4));
 		spawnableCreatureList.add(new SpawnListEntry(EntityCandyPig.class, 8, 4, 6));
