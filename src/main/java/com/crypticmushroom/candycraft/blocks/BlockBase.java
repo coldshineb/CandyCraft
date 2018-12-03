14a136620475002e272352e9f9d194007ee1bef3
StrikerRocker <thariksherief1@gmail.com>
2018-12-03 20:32:43 +0530

    Simplify some block registrations

--- src/main/java/com/crypticmushroom/candycraft/blocks/BlockBase.java
+++ src/main/java/com/crypticmushroom/candycraft/blocks/BlockBase.java
@@ -3,15 +3,22 @@ package com.crypticmushroom.candycraft.blocks;
 import com.crypticmushroom.candycraft.CandyCraft;
 import com.crypticmushroom.candycraft.misc.IModelProvider;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
 import static com.crypticmushroom.candycraft.CandyCraft.MODID;
 
 public class BlockBase extends Block implements IModelProvider {
+    public BlockBase(Material materialIn, String name, SoundType soundType) {
+        this(materialIn, name);
+        setSoundType(soundType);
+    }
     public BlockBase(Material materialIn, String name) {
         super(materialIn);
         setRegistryName(name);
         setTranslationKey(MODID + "." + name);
         setCreativeTab(CandyCraft.ccTab);
+        setResistance(2.5f);
+        setHardness(0.5f);
     }
 }
--- src/main/java/com/crypticmushroom/candycraft/init/CCBlocks.java
+++ src/main/java/com/crypticmushroom/candycraft/init/CCBlocks.java
@@ -6,6 +6,7 @@ import com.crypticmushroom.candycraft.blocks.BlockIceCream;
 import com.crypticmushroom.candycraft.blocks.BlockPudding;
 import com.google.common.collect.Lists;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -18,11 +19,13 @@ import java.util.List;
 
 @Mod.EventBusSubscriber
 public class CCBlocks {
-    public static final Block PUDDING_FLOUR = new BlockPudding("pudding_flour");
-    public static final Block PUDDING_SUGAR = new BlockPudding("pudding_sugar");
+    public static final Block SWEETSTONE = new BlockBase(Material.ROCK, "sweetstone").setHardness(1.5F).setResistance(30.0F);
+    public static final Block PUDDING_BLOCK = new BlockPudding("pudding_block");
+    public static final Block PUDDING_TOP = new BlockPudding("pudding_top");
+    public static final Block SUGAR_BLOCK = new BlockBase(Material.GROUND, "sugar_block", SoundType.SAND);
+    public static final Block FLOUR_BLOCK = new BlockBase(Material.GROUND, "flour_block", SoundType.CLOTH);
     public static final Block MARSHMALLOW_LOG = new BlockCandyLog("marshmallow_log");
-    public static final Block FLOUR_BLOCK = new BlockBase(Material.GROUND, "flour").setHardness(0.6f);
-    public static final Block MARSHMALLOW_PLANK = new BlockBase(Material.WOOD, "marshmallow_plank").setHardness(2.0f);
+    public static final Block MARSHMALLOW_PLANKS = new BlockBase(Material.WOOD, "marshmallow_planks", SoundType.WOOD).setHardness(2.0F).setResistance(15.0F);
     public static final Block ICE_CREAM_BLOCK = new BlockIceCream();
 
     public static List<Block> blocks;
@@ -30,7 +33,7 @@ public class CCBlocks {
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> registryEvent) {
-        blocks = Lists.newArrayList(PUDDING_FLOUR, PUDDING_SUGAR, MARSHMALLOW_LOG, FLOUR_BLOCK, MARSHMALLOW_PLANK);
+        blocks = Lists.newArrayList(SWEETSTONE, PUDDING_BLOCK, PUDDING_TOP, SUGAR_BLOCK, FLOUR_BLOCK, MARSHMALLOW_LOG, MARSHMALLOW_PLANKS);
         blocks.forEach(b -> registryEvent.getRegistry().register(b));
         blockWithSpecialItem = Lists.newArrayList(ICE_CREAM_BLOCK);
         blockWithSpecialItem.forEach(b -> registryEvent.getRegistry().register(b));
