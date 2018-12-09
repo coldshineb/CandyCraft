d4530f4ba06090826f0c66410929ad35a329696f
FlashFyre <33395370+Fla5hFyre@users.noreply.github.com>
2018-12-09 16:26:04 +0000

    Implemented honeycomb block

--- src/main/java/com/crypticmushroom/candycraft/init/CCBlocks.java
+++ src/main/java/com/crypticmushroom/candycraft/init/CCBlocks.java
@@ -34,14 +34,14 @@ public class CCBlocks {
     public static final Block TOFFEESTONE = new BlockBase(Material.ROCK, "toffeestone", 25.0F, 1.4F);
     public static final Block CARAMEL_LOG = new BlockCandyLog("caramel_log");
     public static final Block CARAMEL_PLANKS = new BlockBase(Material.WOOD, "caramel_planks", SoundType.WOOD, 15.0F, 2.0F);
-    public static final Block HONEY_BLOCK = new BlockBase(Material.GROUND, "honey_block", SoundType.SLIME, 2.5F, 0.5F);
-    public static final Block HONEYCOMB_BLOCK = new BlockBase(Material.GROUND, "honeycomb_block", SoundType.CLOTH, 5.0F, 1.0F);
-    public static final Block HONEYSTONE = new BlockBase(Material.ROCK, "honeystone", 25.0F, 1.5F);    
     public static final Block CHOCOLATE_STONE = new BlockBase(Material.ROCK, "chocolate_stone", 30.0F, 1.5F);
     public static final Block CHOCOLATE_PUDDING_BLOCK = new BlockPudding("chocolate_pudding_block");
     public static final Block CHOCOLATE_PUDDING_TOP = new BlockPudding("chocolate_pudding_top");
     public static final Block CHOCOLATE_LOG = new BlockCandyLog("chocolate_log");
     public static final Block CHOCOLATE_PLANKS = new BlockBase(Material.WOOD, "chocolate_planks", SoundType.WOOD, 15.0F, 2.0F);
+    public static final Block HONEY_BLOCK = new BlockBase(Material.GROUND, "honey_block", SoundType.SLIME, 2.5F, 0.5F);
+    public static final Block HONEYCOMB_BLOCK = new BlockBase(Material.GROUND, "honeycomb_block", SoundType.CLOTH, 5.0F, 1.0F);
+    public static final Block HONEYSTONE = new BlockBase(Material.ROCK, "honeystone", 25.0F, 1.5F);
     public static final Block CREAM_BLOCK = new BlockBase(Material.GROUND, "cream_block", 1.0F, 0.2F); 
     public static final Block CREAMSTONE = new BlockBase(Material.GROUND, "creamstone", 2.0F, 0.4F);
     public static final Block ICE_CREAM_BLOCK = new BlockIceCream();
@@ -52,7 +52,7 @@ public class CCBlocks {
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> registryEvent) {
-        blocks = Lists.newArrayList(SWEETSTONE, PUDDING_BLOCK, PUDDING_TOP, SUGAR_BLOCK, FLOUR_BLOCK, MARSHMALLOW_LOG, MARSHMALLOW_PLANKS, CARAMEL_LOG, CARAMEL_PLANKS, CHOCOLATE_LOG, CHOCOLATE_PLANKS);
+        blocks = Lists.newArrayList(SWEETSTONE, PUDDING_BLOCK, PUDDING_TOP, SUGAR_BLOCK, FLOUR_BLOCK, MARSHMALLOW_LOG, MARSHMALLOW_PLANKS, CARAMEL_LOG, CARAMEL_PLANKS, CHOCOLATE_LOG, CHOCOLATE_PLANKS, HONEYCOMB_BLOCK);
         blocks.forEach(b -> registryEvent.getRegistry().register(b));
         blockWithSpecialItem = Lists.newArrayList(ICE_CREAM_BLOCK, GRENADINE_FLUID);
         blockWithSpecialItem.forEach(b -> registryEvent.getRegistry().register(b));
